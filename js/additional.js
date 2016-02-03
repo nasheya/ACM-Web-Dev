136e3aaf884c1769fad58d1ffc48d14ae4523286
nasheya <nasheyarahman@Nasheyas-MacBook-Pro.local>
2016-02-02 20:47:07 -0500

    blehhh

--- js/additional.js
+++ js/additional.js
@@ -1,38 +1,105 @@
-$(document).ready(function(){
-    $(".clickable").click(function(){
-        $(".slider").slideToggle("slow", "swing");
-    });
-});
+$(document).ready(function () {
+      var table = $('#example').DataTable({});
 
-// row.child(format(row.data())).show();
-// tr.addClass('shown');
+      $('#example tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = table.row( tr );
+ 
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            $('div.slider', row.child()).slideUp( function () {
+                row.child.hide();
+                tr.removeClass('shown');
+            } );
+        }
+        else {
+            // Open this row
+            row.child( format(row.data()), 'no-padding' ).show();
+            tr.addClass('shown');
+ 
+            $('div.slider', row.child()).slideDown();
+        }
+    } );
+  });
 
-// $('div.slider', row.child()).slideDown();
+  /* Formatting function for row details - modify as you need */
+function format ( d ) {
+    // `d` is the original data object for the row
+    return '<div class="slider">'+
+        '<table cellpadding="10" cellspacing="2" border="0" style="padding-left:50px;">'+
+            '<tr>'+
+                '<td>Description  </td>'+
+            '</tr>'+
+            '<tr>'+
+                '<td>Aid Requirements:</td>'+
+                '<td>'+d.extn+'</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<td>Service Hours:  </td>'+
+                '<td></td>'+
+                '<td>'+d.extn+'</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<td>Website:  </td>'+
+                '<td>'+d.extn+'</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<td>Address:  </td>'+
+                '<td>'+d.extn+'</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<td>Phone Number:  </td>'+
+                '<td>'+d.name+'</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<td>Email:  </td>'+
+                '<td>'+d.name+'</td>'+
+            '</tr>'+
+            '<tr>'+
+                '<td>Logo </td>'+
+            '</tr>'+
+        '</table>'+
+    '</div>';
+}
+ 
+// $(document).ready(function() {
+//     var table = $('#example').DataTable( {
+//         "ajax": "objects.txt",
+//         "columns": [
+//             {
+//                 "className":      'details-control',
+//                 "orderable":      false,
+//                 "data":           null,
+//                 "defaultContent": ''
+//             },
+//             { "data": "name" },
+//             { "data": "position" },
+//             { "data": "office" },
+//             { "data": "salary" }
+//         ],
+//         "order": [[1, 'asc']]
+//     } );
+     
+//     // Add event listener for opening and closing details
+//     $('#example tbody').on('click', 'td.details-control', function () {
+//         var tr = $(this).closest('tr');
+//         var row = table.row( tr );
+ 
+//         if ( row.child.isShown() ) {
+//             // This row is already open - close it
+//             row.child.hide();
+//             tr.removeClass('shown');
+//         }
+//         else {
+//             // Open this row
+//             row.child( format(row.data()) ).show();
+//             tr.addClass('shown');
+//         }
+//     } );
+// } );
 
-// $('#my_table > tbody > tr.my_row')
-//  .find('td')
-//  .wrapInner('<div style="display: block;" />')
-//  .parent()
-//  .find('td > div')
-//  .slideUp(700, function(){
-
-//   $(this).parent().parent().remove();
-
-//  });
-
-
-
-
-//  $("tr").click(function(){
-//          $('#my_table > tbody > tr.my_row')
-//  .find('td')
-//  .wrapInner('<div style="display: none;" />')
-//  .parent()
-//  .find('td > div')
-//  .slideDown(700, function(){
-
-//   var $set = $(this);
-//   $set.replaceWith($set.contents());
-
-//  });
-//     });
+// // $(document).ready(function(){
+// //     $(".clickable").click(function(NumVal){
+// //         $(".slider").slideDown();
+// //     });
+// // });
