e7df82957a253cdb6a7a4dff4b5f3ac2224bf6af
nasheya <nasheyarahman@Nasheyas-MacBook-Pro.local>
2016-02-08 02:11:23 -0500

    Made all the links work and the table is generated through JavaScript now

--- js/additional.js
+++ js/additional.js
@@ -1,104 +1,57 @@
 $(document).ready(function () {
-      var table = $('#example').DataTable({});
+    var myTableDiv = document.getElementById("my_table")
+    var table = document.createElement('TABLE')
+    var tableBody = document.createElement('TBODY')
+    table.className += "table table-responsive table-hover";
+    table.appendChild(tableBody);
 
-      $('#example tbody').on('click', 'td.details-control', function () {
-        var tr = $(this).closest('tr');
-        var row = table.row( tr );
- 
-        if ( row.child.isShown() ) {
-            // This row is already open - close it
-            $('div.slider', row.child()).slideUp( function () {
-                row.child.hide();
-                tr.removeClass('shown');
-            } );
-        }
-        else {
-            // Open this row
-            row.child( format(row.data()), 'no-padding' ).show();
-            tr.addClass('shown');
- 
-            $('div.slider', row.child()).slideDown();
-        }
-    } );
-  });
+    // var heading = new Array();
+    // heading[0] = "Request Type"
 
-  /* Formatting function for row details - modify as you need */
-function format ( d ) {
-    // `d` is the original data object for the row
-    return '<div class="slider">'+
-        '<table cellpadding="10" cellspacing="2" border="0" style="padding-left:50px;">'+
-            '<tr>'+
-                '<td>Description  </td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td>Aid Requirements:</td>'+
-                '<td>'+d.extn+'</td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td>Service Hours:  </td>'+
-                '<td>'+d.extn+'</td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td>Website:  </td>'+
-                '<td>'+d.extn+'</td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td>Address:  </td>'+
-                '<td>'+d.extn+'</td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td>Phone Number:  </td>'+
-                '<td>'+d.name+'</td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td>Email:  </td>'+
-                '<td>'+d.name+'</td>'+
-            '</tr>'+
-            '<tr>'+
-                '<td>Logo </td>'+
-            '</tr>'+
-        '</table>'+
-    '</div>';
-}
- 
-// $(document).ready(function() {
-//     var table = $('#example').DataTable( {
-//         "ajax": "objects.txt",
-//         "columns": [
-//             {
-//                 "className":      'details-control',
-//                 "orderable":      false,
-//                 "data":           null,
-//                 "defaultContent": ''
-//             },
-//             { "data": "name" },
-//             { "data": "position" },
-//             { "data": "office" },
-//             { "data": "salary" }
-//         ],
-//         "order": [[1, 'asc']]
-//     } );
-     
-//     // Add event listener for opening and closing details
-//     $('#example tbody').on('click', 'td.details-control', function () {
-//         var tr = $(this).closest('tr');
-//         var row = table.row( tr );
- 
-//         if ( row.child.isShown() ) {
-//             // This row is already open - close it
-//             row.child.hide();
-//             tr.removeClass('shown');
-//         }
-//         else {
-//             // Open this row
-//             row.child( format(row.data()) ).show();
-//             tr.addClass('shown');
-//         }
-//     } );
-// } );
+    var stock = new Array()
+    stock[0] = new Array("Cars", "88.625", "85.50", "85.81", "987")
+    stock[1] = new Array("Veggies", "88.62dfdf", "85.50", "85.81", "988")
+    stock[2] = new Array("Colors", "88.62dcolors5", "85.50", "85.81", "989")
+    stock[3] = new Array("Numbers", "88.6num25", "85.50", "85.81", "990")
+    stock[4] = new Array("Requests", "88.6rew25", "85.50", "85.81", "991")
 
-// // $(document).ready(function(){
-// //     $(".clickable").click(function(NumVal){
-// //         $(".slider").slideDown();
-// //     });
-// // });
+    //TABLE COLUMNS
+    // var tr = document.createElement('TR');
+    // tableBody.appendChild(tr);
+    // for (i = 0; i < stock.length; i++) {
+    //     var th = document.createElement('TH')
+    //     th.width = '75';
+    //     th.appendChild(document.createTextNode(stock[i][0]));
+    //     tr.appendChild(th);
+    // }
+
+    //TABLE ROWS
+    for (i = 0; i < stock.length; i++) {
+        var tr = document.createElement('TR');
+        tr.className += "clickable";
+        var id = "row".concat(i);
+        tr.id = id;
+        var target = ".".concat(id);
+        tr.setAttribute("data-toggle", "collapse")
+        tr.setAttribute("data-target", target)
+
+        // for (j = 0; j < stock[i].length; j++) {
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(stock[i][0]));
+
+            tr.appendChild(td);
+        // }
+
+        tableBody.appendChild(tr);
+
+        var tr2 = document.createElement('TR');
+        tr2.className += "collapse ".concat(id);
+        var td2 = document.createElement('TD');
+            td2.appendChild(document.createTextNode(stock[i][1]));
+            tr2.appendChild(td2);
+
+        tableBody.appendChild(tr2);
+    }
+
+    myTableDiv.appendChild(table)
+});
